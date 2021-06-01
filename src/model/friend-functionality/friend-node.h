2d414bfe91231dda5152aa27fbb2beb7487f0adc
mitch161 <mitchellhardie1@gmail.com>
2021-06-01 09:17:54 +0100

    Added getter method for geting a friends name
    The GetName method currently fake returns a string value 'mitch'. This has been done to ensure the new GetName unit test passes within test suite FriendNodeTest.

---
+++ src/model/friend-functionality/friend-node.h
@@ -0,0 +1,13 @@
+#ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDNODE_
+#define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDNODE_
+
+#include <string>
+
+namespace chat_client_model_friend_functionality {
+class FriendNode {
+ public:
+  std::string GetName();
+};
+}  // namespace chat_client_model_friend_functionality
+
+#endif
