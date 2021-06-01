f98fe5c58b760984ff80e180cc7c28dc0d4654c9
mitch161 <mitchellhardie1@gmail.com>
2021-06-01 10:50:32 +0100

    Refactored constructor to take a reference
    These changes have been made so the address of the name can be passed, avoiding unnecessary copies of the name attribute. A dereference occurs internally to ensure that a new copy is contained within the object.

--- src/model/friend-functionality/friend-node.cc
+++ src/model/friend-functionality/friend-node.cc
@@ -4,6 +4,6 @@
 
 using namespace chat_client_model_friend_functionality;
 
-FriendNode::FriendNode(std::string name) { this->name = name; }
+FriendNode::FriendNode(std::string *name) { this->name = *name; }
 
 std::string FriendNode::GetName() { return this->name; }
