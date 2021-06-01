fb35e0738afde7146852d76d7d299e9b3c4090c2
mitch161 <mitchellhardie1@gmail.com>
2021-06-01 10:14:47 +0100

    Added test to get multiple names
    This unit test uses two separate FriendNode objects to get their names and ensure they match the corresponding local variables. The purpose of this test is to allow FriendNodes to return different names, rather than faked values.

--- test/model/friend-functionality/friend-node_test.cc
+++ test/model/friend-functionality/friend-node_test.cc
@@ -7,11 +7,26 @@
 using namespace chat_client_model_friend_functionality;
 
 class FriendNodeTest : public ::testing::Test {
+ public:
+  FriendNodeTest() {
+    friend_one = new FriendNode("mitch");
+    friend_two = new FriendNode("micky");
+  }
+
  protected:
-  FriendNode friend_one;
+  FriendNode *friend_one;
+  FriendNode *friend_two;
 };
 
 TEST_F(FriendNodeTest, GetName) {
   std::string name = "mitch";
-  EXPECT_EQ(friend_one.GetName(), name);
+  EXPECT_EQ(friend_one->GetName(), name);
+}
+
+TEST_F(FriendNodeTest, GetNameDouble) {
+  std::string name_one = "mitch";
+  std::string name_two = "micky";
+
+  EXPECT_EQ(friend_one->GetName(), name_one);
+  EXPECT_EQ(friend_two->GetName(), name_two);
 }
