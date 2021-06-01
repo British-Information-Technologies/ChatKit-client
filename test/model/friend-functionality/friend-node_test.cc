2db3059dcb8a9983678f39fb57e44b182079d7ed
mitch161 <mitchellhardie1@gmail.com>
2021-06-01 11:17:21 +0100

    Added two name attributes for test access

--- test/model/friend-functionality/friend-node_test.cc
+++ test/model/friend-functionality/friend-node_test.cc
@@ -17,19 +17,15 @@ class FriendNodeTest : public ::testing::Test {
   }
 
  protected:
+  std::string name_one = "mitch";
+  std::string name_two = "micky";
   FriendNode *friend_one;
   FriendNode *friend_two;
 };
 
-TEST_F(FriendNodeTest, GetName) {
-  std::string name = "mitch";
-  EXPECT_EQ(friend_one->GetName(), name);
-}
+TEST_F(FriendNodeTest, GetName) { EXPECT_EQ(friend_one->GetName(), name_one); }
 
 TEST_F(FriendNodeTest, GetNameDouble) {
-  std::string name_one = "mitch";
-  std::string name_two = "micky";
-
   EXPECT_EQ(friend_one->GetName(), name_one);
   EXPECT_EQ(friend_two->GetName(), name_two);
 }
