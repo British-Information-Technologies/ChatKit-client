8b7fe5f01048ab493fed8d27832bbe1c60bacdbe
mitch161 <mitchellhardie1@gmail.com>
2021-06-04 09:20:30 +0100

    Added AddFriend function with faked true return
    These changes add the ability to add friends to the FriendHashmap by calling the add friend function. Currently, this function returns a fake true value, simulating the behaviour of adding friends to the map. These changes have been made to satisfy the AddFriendTrue unit test within the FriendHashmapTest suite.

--- src/model/friend-functionality/friend-hashmap.cc
+++ src/model/friend-functionality/friend-hashmap.cc
@@ -1,1 +1,7 @@
 #include "friend-hashmap.h"
+
+#include "friend-node.h"
+
+using namespace chat_client_model_friend_functionality;
+
+bool FriendHashmap::AddFriend(FriendNode friend_node) { return true; }
--- src/model/friend-functionality/friend-hashmap.h
+++ src/model/friend-functionality/friend-hashmap.h
@@ -1,8 +1,13 @@
 #ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDHASHMAP_
 #define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDHASHMAP_
 
+#include "friend-node.h"
+
 namespace chat_client_model_friend_functionality {
-class FriendHashmap {};
+class FriendHashmap {
+ public:
+  bool AddFriend(FriendNode);
+};
 }  // namespace chat_client_model_friend_functionality
 
 #endif
