ff03999729d2889af7682de400b34330552c6ba1
mitch161 <mitchellhardie1@gmail.com>
2021-06-04 13:15:23 +0100

    Replaced array with map and updated AddFriend
    These changes have replaced the fixed sized array with a dynamic map that stores friend nodes indexed by their uuids. This was done to ensure an arbitrary amount of friends can be added, while allowing each one to be checked if it already exists in the map. This provides a more dynamic and scable approach to managing friends.

--- src/model/friend-functionality/friend-hashmap.h
+++ src/model/friend-functionality/friend-hashmap.h
@@ -1,13 +1,15 @@
 #ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDHASHMAP_
 #define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDHASHMAP_
 
+#include <map>
+#include <string>
+
 #include "friend-node.h"
 
 namespace chat_client_model_friend_functionality {
 class FriendHashmap {
  private:
-  FriendNode prev[2];
-  int pos = 0;
+  std::map<std::string, FriendNode> friend_map;
 
  public:
   bool AddFriend(FriendNode);
