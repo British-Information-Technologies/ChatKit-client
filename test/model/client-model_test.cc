ba0a9dc3e131c54eac626369f068de5b767b8f16
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 20:18:35 +0100

    Added test to request a single friend

--- test/model/client-model_test.cc
+++ test/model/client-model_test.cc
@@ -2,6 +2,23 @@
 
 #include <gtest/gtest.h>
 
+#include <string>
+
+#include "model/friend-functionality/friend-node.h"
+
 using namespace chat_client_model;
+using namespace chat_client_model_friend_functionality;
 
 class FriendClientModelTest : public ::testing::Test {};
+
+TEST_F(FriendClientModelTest, AddFriendTrue) {
+  ClientModel model;
+  std::string name = "mitch";
+  std::string uuid = "test";
+
+  FriendNode *friend_one = new FriendNode(&name, &uuid);
+
+  EXPECT_TRUE(model.AddFriend(*friend_one));
+
+  delete friend_one;
+}
