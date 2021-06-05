6d9053ab064b6213bfc9badc96b41ab945ea468f
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 20:19:59 +0100

    Created AddFriend method with fake return
    These changes add a new method AddFriend that fakes a return of true, allowing the unit test AddFriendTrue within the FriendClientModelTest suite to pass.

---
+++ src/model/client-model.cc
@@ -0,0 +1,5 @@
+#include "client-model.h"
+
+using namespace chat_client_model;
+
+bool ClientModel::AddFriend(FriendNode& friend_node) { return true; }
---
+++ src/model/client-model.h
@@ -0,0 +1,15 @@
+#ifndef CPPCHATCLIENT_MODEL_CLIENTMODEL_
+#define CPPCHATCLIENT_MODEL_CLIENTMODEL_
+
+#include "friend-functionality/friend-node.h"
+
+using namespace chat_client_model_friend_functionality;
+
+namespace chat_client_model {
+class ClientModel {
+ public:
+  bool AddFriend(FriendNode&);
+};
+}  // namespace chat_client_model
+
+#endif
