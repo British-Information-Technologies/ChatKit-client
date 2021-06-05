16d106f4d1b586bf46039a9f7c12b49eee06f434
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 22:00:26 +0100

    Added test to add node to friend list
    The purpose of this test is to ensure that friend nodes can be successfully added to the friend list data structure.

--- test/model/friend-functionality/friend-utility_test.cc
+++ test/model/friend-functionality/friend-utility_test.cc
@@ -4,4 +4,17 @@
 
 #include <string>
 
+#include "model/friend-functionality/friend-node.h"
+
+using namespace chat_client_model_friend_functionality;
+
 class FriendUtilityTest : public ::testing::Test {};
+
+TEST_F(FriendUtilityTest, AddFriendTrue) {
+  FriendUtility utility;
+  std::string name = "mitch";
+  std::string uuid_one = "test";
+  FriendNode *friend_one = new FriendNode(&name, &uuid_one);
+
+  EXPECT_TRUE(utility.AddFriend(*friend_one));
+}
