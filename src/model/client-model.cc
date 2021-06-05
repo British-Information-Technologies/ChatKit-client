bf826dbb24452485ca958178f98cdc5a4d33fc6d
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 20:38:30 +0100

    Updated AddFriend to store last added node
    The AddFriend method now stores the last added node to make sure the next is not a duplicate. The correct logic has now replaced the fake return of true to accomidate for this change.

--- src/model/client-model.cc
+++ src/model/client-model.cc
@@ -2,4 +2,11 @@
 
 using namespace chat_client_model;
 
-bool ClientModel::AddFriend(FriendNode& friend_node) { return true; }
+bool ClientModel::AddFriend(FriendNode& friend_node) {
+  if (prev.GetUuid() != friend_node.GetUuid()) {
+    prev = friend_node;
+    return true;
+  }
+
+  return false;
+}
