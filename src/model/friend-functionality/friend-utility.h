55ad9a747abc8b5f12de9fdc7138275c4897ba5d
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 22:04:08 +0100

    Added fake return method AddFriend
    These changes add a method that fakes a return of true, allowing the test AddFriendTrue within the FriendUtilityTest suite

--- src/model/friend-functionality/friend-utility.h
+++ src/model/friend-functionality/friend-utility.h
@@ -1,4 +1,12 @@
 #ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDUTILITY_
 #define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDUTILITY_
 
+#include "friend-node.h"
+namespace chat_client_model_friend_functionality {
+class FriendUtility {
+ public:
+  bool AddFriend(FriendNode&);
+};
+}  // namespace chat_client_model_friend_functionality
+
 #endif
