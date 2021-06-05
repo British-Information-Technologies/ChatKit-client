4f948c7772737cc1220ece633424d7e4897b6fb8
mitch161 <mitchellhardie1@gmail.com>
2021-06-05 20:38:30 +0100

    Updated AddFriend to store last added node
    The AddFriend method now stores the last added node to make sure the next is not a duplicate. The correct logic has now replaced the fake return of true to accomidate for this change.

--- src/model/client-model.h
+++ src/model/client-model.h
@@ -7,6 +7,9 @@ using namespace chat_client_model_friend_functionality;
 
 namespace chat_client_model {
 class ClientModel {
+ private:
+  FriendNode prev;
+
  public:
   bool AddFriend(FriendNode&);
 };
