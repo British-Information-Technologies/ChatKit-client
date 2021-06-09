f056594f964f1747c548d26b57285c298e0b2da0
mitch161 <mitchellhardie1@gmail.com>
2021-06-09 21:53:48 +0100

    Added test to add double friend nodes
    The purpose of this test is to ensure that more than one friend node can be request to be added.

--- test/model/friend-functionality/friend-utility_test.cc
+++ test/model/friend-functionality/friend-utility_test.cc
@@ -8,13 +8,33 @@
 
 using namespace chat_client_model_friend_functionality;
 
-class FriendUtilityTest : public ::testing::Test {};
+class FriendUtilityTest : public ::testing::Test {
+ public:
+  FriendUtilityTest() {
+    name = "mitch";
+    uuid_one = "test";
+    uuid_two = "less";
+    friend_one = new FriendNode(&name, &uuid_one);
+    friend_two = new FriendNode(&name, &uuid_two);
+  }
+
+ protected:
+  std::string name;
+  std::string uuid_one;
+  std::string uuid_two;
+  FriendNode *friend_one;
+  FriendNode *friend_two;
+};
 
 TEST_F(FriendUtilityTest, AddFriendTrue) {
   FriendUtility utility;
-  std::string name = "mitch";
-  std::string uuid_one = "test";
-  FriendNode *friend_one = new FriendNode(&name, &uuid_one);
 
   EXPECT_TRUE(utility.AddFriend(*friend_one));
+}
+
+TEST_F(FriendUtilityTest, AddFriendDoubleTrue) {
+  FriendUtility utility;
+
+  EXPECT_TRUE(utility.AddFriend(*friend_one));
+  EXPECT_TRUE(utility.AddFriend(*friend_two));
 }
