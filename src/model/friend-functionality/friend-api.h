53a2b6851c266844a0106925bf65fbf59cd923ad
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 09:31:00 +0100

    Changed private to protected
    These changes where made so that the attributes of the abstract class could be successfully accessed by the implementors.

--- src/model/friend-functionality/friend-api.h
+++ src/model/friend-functionality/friend-api.h
@@ -1,16 +1,17 @@
 #ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDAPI_
 #define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDAPI_
 
+#include "friend-hashmap.h"
 #include "friend-list.h"
 
 namespace chat_client_model_friend_functionality {
 class FriendAPI {
- private:
-  FriendList friend_list;
-
  public:
-  virtual bool AddFriend(FriendNode&) = 0;
-  virtual shared_ptr<FriendNode> GetFriend(const string&) const = 0;
+  virtual bool AddFriend(FriendNode &) = 0;
+  virtual shared_ptr<FriendNode> GetFriend(const string &) const = 0;
+
+ protected:
+  FriendList *friend_list = new FriendHashmap();
 };
 }  // namespace chat_client_model_friend_functionality
 
