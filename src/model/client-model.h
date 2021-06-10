5a982bdcc1842591644e2b7201d4b3ad89d5f992
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 11:05:30 +0100

    Updated to use FriendAPI to add friends
    These changes have added a new private attribute containing the FriendUtility abstracted away under the FriendAPI. This allows the model to make calls to add to the internal data structure without having to worry about how the code works under the hood.

--- src/model/client-model.h
+++ src/model/client-model.h
@@ -1,18 +1,19 @@
 #ifndef CPPCHATCLIENT_MODEL_CLIENTMODEL_
 #define CPPCHATCLIENT_MODEL_CLIENTMODEL_
 
+#include "friend-functionality/friend-api.h"
 #include "friend-functionality/friend-node.h"
+#include "friend-functionality/friend-utility.h"
 
 using namespace chat_client_model_friend_functionality;
 
 namespace chat_client_model {
 class ClientModel {
  private:
-  FriendNode prev[2];
-  int pos = 0;
+  FriendAPI *friend_api = new FriendUtility();
 
  public:
-  bool AddFriend(FriendNode&);
+  bool AddFriend(FriendNode &);
 };
 }  // namespace chat_client_model
 
