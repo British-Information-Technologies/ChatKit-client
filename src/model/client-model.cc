51f52174e7cdbaa3e451f87a2be6b84c0df2c8fe
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 11:05:30 +0100

    Updated to use FriendAPI to add friends
    These changes have added a new private attribute containing the FriendUtility abstracted away under the FriendAPI. This allows the model to make calls to add to the internal data structure without having to worry about how the code works under the hood.

--- src/model/client-model.cc
+++ src/model/client-model.cc
@@ -3,12 +3,5 @@
 using namespace chat_client_model;
 
 bool ClientModel::AddFriend(FriendNode& friend_node) {
-  if (prev[0].GetUuid() != friend_node.GetUuid() &&
-      prev[1].GetUuid() != friend_node.GetUuid()) {
-    prev[pos % 2] = friend_node;
-    ++pos;
-    return true;
-  }
-
-  return false;
+  return friend_api->AddFriend(friend_node);
 }
