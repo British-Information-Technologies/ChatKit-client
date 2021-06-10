a48c88b00e58b46f59cb77c13c99e2680c26a95b
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 09:46:39 +0100

    Added getter method to return fake node

--- src/model/friend-functionality/friend-utility.cc
+++ src/model/friend-functionality/friend-utility.cc
@@ -1,6 +1,9 @@
 #include "friend-utility.h"
 
 #include <memory>
+#include <string>
+
+#include "friend-node.h"
 
 using namespace chat_client_model_friend_functionality;
 
@@ -8,4 +11,11 @@ using namespace std;
 
 bool FriendUtility::AddFriend(FriendNode& friend_node) {
   return friend_list->AddFriend(friend_node);
+}
+
+shared_ptr<FriendNode> FriendUtility::GetFriend(const string& uuid) const {
+  string name = "test";
+  FriendNode friend_node(&name, &name);
+  shared_ptr<FriendNode> tmp = make_shared<FriendNode>(friend_node);
+  return tmp;
 }
