1d6c05bf4a49a971031a33b9dadb2c2b627bfdb8
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 08:30:00 +0100

    Modified prev attribute to array
    These changes have been made as it allows two friend nodes to be checked for duplication. Instead of using the previously added friend node, the last two can now be checked.

--- src/model/friend-functionality/friend-utility.cc
+++ src/model/friend-functionality/friend-utility.cc
@@ -3,8 +3,10 @@
 using namespace chat_client_model_friend_functionality;
 
 bool FriendUtility::AddFriend(FriendNode& friend_node) {
-  if (prev.GetUuid() != friend_node.GetUuid()) {
-    prev = friend_node;
+  if (prev[0].GetUuid() != friend_node.GetUuid() &&
+      prev[1].GetUuid() != friend_node.GetUuid()) {
+    prev[pos % 2] = friend_node;
+    ++pos;
     return true;
   }
   return false;
