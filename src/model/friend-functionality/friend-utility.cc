e7c4d0b2372933eb860e36497cad1517207ac90e
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 09:33:55 +0100

    Implemented FriendAPI and called AddFriend
    This class now subclasses the abstract class FriendAPI to allow it to use the features of the FriendList abstract class. Giving the ability to make calls of the methods on the friend hashmap.

--- src/model/friend-functionality/friend-utility.cc
+++ src/model/friend-functionality/friend-utility.cc
@@ -1,13 +1,11 @@
 #include "friend-utility.h"
 
+#include <memory>
+
 using namespace chat_client_model_friend_functionality;
 
+using namespace std;
+
 bool FriendUtility::AddFriend(FriendNode& friend_node) {
-  if (prev[0].GetUuid() != friend_node.GetUuid() &&
-      prev[1].GetUuid() != friend_node.GetUuid()) {
-    prev[pos % 2] = friend_node;
-    ++pos;
-    return true;
-  }
-  return false;
+  return friend_list->AddFriend(friend_node);
 }
--- src/model/friend-functionality/friend-utility.h
+++ src/model/friend-functionality/friend-utility.h
@@ -1,15 +1,16 @@
 #ifndef CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDUTILITY_
 #define CPPCHATCLIENT_MODEL_FRIENDFUNCTIONALITY_FRIENDUTILITY_
 
-#include "friend-node.h"
-namespace chat_client_model_friend_functionality {
-class FriendUtility {
- private:
-  FriendNode prev[2];
-  int pos = 0;
+#include <memory>
 
+#include "friend-api.h"
+#include "friend-node.h"
+
+using namespace std;
+namespace chat_client_model_friend_functionality {
+class FriendUtility : public FriendAPI {
  public:
-  bool AddFriend(FriendNode&);
+  bool AddFriend(FriendNode &);
 };
 }  // namespace chat_client_model_friend_functionality
 
