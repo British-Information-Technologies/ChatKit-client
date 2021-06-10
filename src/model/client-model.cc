e99833acdfafc899064d2327b2d13de22b09333a
mitch161 <mitchellhardie1@gmail.com>
2021-06-10 12:49:09 +0100

    Updated GetFriend to use FriendAPI for real results
    This change allows friends to be retrieved from the friendlist data structures.

--- src/model/client-model.cc
+++ src/model/client-model.cc
@@ -7,8 +7,5 @@ bool ClientModel::AddFriend(FriendNode& friend_node) {
 }
 
 shared_ptr<FriendNode> ClientModel::GetFriend(const string& uuid) const {
-  string name = "test";
-  FriendNode friend_node(&name, &name);
-  shared_ptr<FriendNode> tmp = make_shared<FriendNode>(friend_node);
-  return tmp;
+  return friend_api->GetFriend(uuid);
 }
