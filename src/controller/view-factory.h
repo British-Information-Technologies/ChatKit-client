c7c1c709e238ebddd7434fc339db0d9013a6b3c1
mitch161 <mitchellhardie1@gmail.com>
2021-06-18 16:47:00 +0100

    Changed unique_ptr to shared_ptr
    These changes have been made to allow the gui view to be shared with all observers.

--- src/controller/view-factory.h
+++ src/controller/view-factory.h
@@ -1,12 +1,14 @@
 #ifndef CPPCHATCLIENT_CONTROLLER_VIEWFACTORY_H_
 #define CPPCHATCLIENT_CONTROLLER_VIEWFACTORY_H_
 
+#include <memory>
+
 #include "../view/view.h"
 
 namespace chat_client_controller {
 class ViewFactory {
  public:
-  std::unique_ptr<chat_client_view::View> CreateView(int, char **);
+  std::shared_ptr<chat_client_view::View> CreateView(int, char **);
 };
 }  // namespace chat_client_controller
 
