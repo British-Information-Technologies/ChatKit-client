d52f4a715ce4b409277519f1f4eaba641775569d
mitch161 <mitchellhardie1@gmail.com>
2021-06-18 16:45:13 +0100

    Added view subject to attach to
    These changes have added a new attribute that stores the view as the subject, allowing for any concrete observers to attach to.

--- src/controller/observer.h
+++ src/controller/observer.h
@@ -1,10 +1,19 @@
 #ifndef CPPCHATCLIENT_CONTROLLER_OBSERVER_H_
 #define CPPCHATCLIENT_CONTROLLER_OBSERVER_H_
 
+#include <memory>
+
+namespace chat_client_view {
+class View;
+}
+
 namespace chat_client_controller {
 class Observer {
+ protected:
+  std::shared_ptr<chat_client_view::View> view;
+
  public:
-  virtual void AddFriend() = 0;
+  virtual void Execute() = 0;
 };
 }  // namespace chat_client_controller
 
