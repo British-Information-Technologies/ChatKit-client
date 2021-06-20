f567c81fee40aa2addb8f216dec4b26299db950d
mitch161 <mitchellhardie1@gmail.com>
2021-06-20 19:01:54 +0100

    Added DeleteFriend method to delete nodes

--- src/model/client-model.cc
+++ src/model/client-model.cc
@@ -7,6 +7,8 @@ bool ClientModel::AddFriend(const string& uuid) {
   return friend_api->AddFriend(uuid);
 }
 
+bool ClientModel::DeleteFriend(const std::string& uuid) { return true; }
+
 shared_ptr<FriendNode> ClientModel::GetFriend(const string& uuid) const {
   return friend_api->GetFriend(uuid);
 }
