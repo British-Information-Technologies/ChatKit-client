2d0a6763c4a946b7e7c70b079dd37afdfa7f9b55
mitch161 <mitchellhardie1@gmail.com>
2021-06-24 21:59:55 +0100

    Fixed build errors with gktmm4.0
    These changes have stopped build and syntax errors occuring with the new gtkmm4.0 include.

--- src/MainApplication.cc
+++ src/MainApplication.cc
@@ -5,17 +5,18 @@
 /*
  *
  */
-MainApplication::MainApplication() : Application("org.gtkmm.examples.application") {
-  // auto builder = Builder::create();
-  // builder->add_from_file("resources/mickyb18-chat-client.glade");
-  // this->mainWindow = builder->get_widget<Window>("main_window");
+MainApplication::MainApplication()
+    : Application("org.gtkmm.examples.application") {
+  builder = Builder::create();
+  builder->add_from_file("resources/mickyb18-chat-client.glade");
 
-
-  // this->mainWindow->show();
+  main_window = builder->get_widget<Gtk::Widget>("main_window");
 }
 
 MainApplication::~MainApplication() { this->release(); }
 
 Glib::RefPtr<MainApplication> MainApplication::create() {
-	return Glib::make_refptr_for_instance<MainApplication>(new MainApplication());
+  return Glib::make_refptr_for_instance<MainApplication>(new MainApplication());
 }
+
+void MainApplication::on_activate() { main_window->show(); }
