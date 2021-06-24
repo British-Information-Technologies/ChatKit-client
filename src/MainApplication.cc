fd2c8822b7dd160f478c82faa2e1d517e31e90d7
mitch161 <mitchellhardie1@gmail.com>
2021-06-24 20:19:52 +0100

    Updated include and method calls for gtkmm 4.0

--- src/MainApplication.cc
+++ src/MainApplication.cc
@@ -1,23 +1,19 @@
-#include <stdio.h>
-
 #include "MainApplication.h"
+
+#include <stdio.h>
 
 /*
  *
  */
-MainApplication::MainApplication()
-	: Application() {
+MainApplication::MainApplication() : Application() {
+  Window* mainWindow = nullptr;
 
-	Window* mainWindow = nullptr;
+  auto builder = Builder::create();
+  builder->add_from_file("resource/mickyb18-chat-client.glade");
+  builder->get_widget("main_window", mainWindow);
 
-	auto builder = Builder::create();
-	builder->add_from_file("resource/mickyb18-chat-client.glade");
-	builder->get_widget("main_window", mainWindow);
-
-	this->mainWindow = mainWindow;
-	this->mainWindow->show();
+  this->mainWindow = mainWindow;
+  this->mainWindow->show_all();
 }
 
-MainApplication::~MainApplication() {
-	this->release();
-}
+MainApplication::~MainApplication() { this->release(); }
