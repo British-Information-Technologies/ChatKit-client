d9318f070c23de20d57e8cc5801152e4596e0362
mitch161 <mitchellhardie1@gmail.com>
2021-06-24 21:16:08 +0100

    Created main window display class
    This will be the main window that will be contained inside the main application. It can be shown and hidden on command in the future

---
+++ src/main-window.cc
@@ -0,0 +1,5 @@
+#include "main-window.h"
+
+using namespace chat_client_view;
+
+MainWindow::MainWindow() : Gtk::Window() {}
---
+++ src/main-window.h
@@ -0,0 +1,14 @@
+#ifndef CPPCHATCLIENT_MAIN_WINDOW_H
+#define CPPCHATCLIENT_MAIN_WINDOW_H
+
+#include <gtkmm-4.0/gtkmm.h>
+
+namespace chat_client_view {
+class MainWindow : public Gtk::Window {
+ public:
+  MainWindow();
+};
+
+}  // namespace chat_client_view
+
+#endif
