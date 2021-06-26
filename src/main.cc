d75ad1c2f461d01e10f6698db6fec8ffb21a6c70
mitch161 <mitchellhardie1@gmail.com>
2021-06-26 22:14:15 +0100

    Formatted file to google standard

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,17 @@
-#include "controller/client-controller.h"
+#include <iostream>
+
 #include "MainApplication.h"
+#include "controller/client-controller.h"
 
 using namespace chat_client_controller;
 
 int main(int argc, char **argv) {
-
-	// create application
+  // create application
   auto app = MainApplication::create();
 
-	// increases reference count.
-	app->hold();
+  // increases reference count.
+  app->hold();
 
-	// enter main loop
-	app->run(argc, argv);
+  // enter main loop
+  app->run(argc, argv);
 }
