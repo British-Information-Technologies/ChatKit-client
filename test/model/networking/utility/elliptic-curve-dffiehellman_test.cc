682d6748ce788e95f3497b285a1250de6cdef368
mitch161 <mitchellhardie1@gmail.com>
2021-07-04 10:21:38 +0100

    Added tests for ECDH key exchange
    These inital tests ensure that key exchange protocol works as intended and produces the right results.

---
+++ test/model/networking/utility/elliptic-curve-dffiehellman_test.cc
@@ -0,0 +1,83 @@
+#include "model/networking/utility/elliptic-curve-dffiehellman.h"
+
+#include <gtest/gtest.h>
+
+using namespace key_agreement;
+
+TEST(EllipticCurveDiffieHellmanTest, CreateKeyPair) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EXPECT_NE(alice_private_key, nullptr);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, ExtractPublicKeyFromPrivateKey) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+
+  EXPECT_NE(alice_private_key, nullptr);
+  EXPECT_NE(alice_public_key, nullptr);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, GenerateDifferentKeyPairs) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+  EXPECT_NE(alice_private_key.get(), bob_private_key.get());
+}
+
+TEST(EllipticCurveDiffieHellmanTest, ExtractDifferentPublicKeys) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+  EXPECT_NE(alice_private_key, nullptr);
+  EXPECT_NE(bob_private_key, nullptr);
+
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+  EVP_PKEY_free_ptr bob_public_key = ExtractPublicKey(bob_private_key.get());
+
+  EXPECT_NE(alice_public_key, nullptr);
+  EXPECT_NE(bob_public_key, nullptr);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, CreateSharedSecret) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+  EXPECT_NE(alice_private_key, nullptr);
+  EXPECT_NE(bob_private_key, nullptr);
+
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+  EVP_PKEY_free_ptr bob_public_key = ExtractPublicKey(bob_private_key.get());
+
+  EXPECT_NE(alice_public_key, nullptr);
+  EXPECT_NE(bob_public_key, nullptr);
+
+  derivedKey* secret_alice =
+      DeriveSharedSecret(bob_public_key.get(), alice_private_key.get());
+  derivedKey* secret_bob =
+      DeriveSharedSecret(alice_public_key.get(), bob_private_key.get());
+
+  // turn shared secret into a displayable format
+  BIGNUM* secret_alice_BN = BN_new();
+  BIGNUM* secret_bob_BN = BN_new();
+
+  BN_bin2bn(secret_alice->secret, secret_alice->length, secret_alice_BN);
+  BN_bin2bn(secret_bob->secret, secret_bob->length, secret_bob_BN);
+
+  std::cout << "Secret computed by Alice: ";
+  BN_print_fp(stdout, secret_alice_BN);
+  std::cout << std::endl;
+
+  std::cout << "Secret computed by Bob: ";
+  BN_print_fp(stdout, secret_bob_BN);
+  std::cout << std::endl;
+
+  if (BN_cmp(secret_alice_BN, secret_bob_BN) == 0) {
+    printf("\n\nSecrets computed were equal! Magic of ECDH\n\n");
+  }
+
+  BN_free(secret_alice_BN);
+  BN_free(secret_bob_BN);
+}
