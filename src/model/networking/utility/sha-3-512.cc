f53ffb03ff7c0fe9c96de5d9ad1512ab6563c026
mitch161 <mitchellhardie1@gmail.com>
2021-07-07 08:29:04 +0100

    Added SHA3 functionality
    These changes will allow shared secrets to be hashed to add extra security.

---
+++ src/model/networking/utility/sha-3-512.cc
@@ -0,0 +1,52 @@
+#include "sha-3-512.h"
+
+#include <openssl/evp.h>
+
+#include <cstring>
+#include <iostream>
+#include <memory>
+
+using namespace networking_utility;
+
+extern "C" {
+
+using EVP_MD_CTX_free_ptr =
+    std::unique_ptr<EVP_MD_CTX, decltype(&::EVP_MD_CTX_free)>;
+
+void HashData(DerivedData *data) {
+  const EVP_MD *hashing_algorithm = EVP_sha3_512();
+  EVP_MD_CTX_free_ptr mdctx(EVP_MD_CTX_create(), ::EVP_MD_CTX_free);
+
+  // initialize digest engine
+  if (EVP_DigestInit_ex(mdctx.get(), hashing_algorithm, NULL) != 1) {
+    abort();
+  }
+
+  // provide data to digest engine
+  if (EVP_DigestUpdate(mdctx.get(), data->secret, data->length) != 1) {
+    abort();
+  }
+
+  unsigned int digest_length = EVP_MD_size(hashing_algorithm);
+  unsigned char *digest = (unsigned char *)malloc(digest_length);
+  if (digest == NULL) {
+    abort();
+  }
+
+  // produce digest
+  if (EVP_DigestFinal_ex(mdctx.get(), digest, &digest_length) != 1) {
+    free(digest);
+    abort();
+  }
+
+  data->length = digest_length;
+  free(data->secret);
+  if (NULL == (data->secret = (unsigned char *)malloc(data->length))) {
+    free(digest);
+    abort();
+  }
+
+  strcpy((char *)data->secret, (const char *)digest);
+  free(digest);
+}
+}
---
+++ src/model/networking/utility/sha-3-512.h
@@ -0,0 +1,11 @@
+#ifndef CPPCHATCLIENT_SHA_3_512_H_
+#define CPPCHATCLIENT_SHA_3_512_H_
+
+#include "derived-data.h"
+
+namespace networking_utility {
+
+void HashData(DerivedData *);
+
+}  // namespace networking_utility
+#endif
