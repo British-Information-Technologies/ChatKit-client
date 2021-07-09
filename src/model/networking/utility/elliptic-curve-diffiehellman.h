4c3cc1b7f71d5945c5745aa9dc1e6c0ba7990969
mitch161 <mitchellhardie1@gmail.com>
2021-07-09 08:59:38 +0100

    Removed parameter names to match other files

--- src/model/networking/utility/elliptic-curve-diffiehellman.h
+++ src/model/networking/utility/elliptic-curve-diffiehellman.h
@@ -12,8 +12,8 @@ namespace networking_utility {
 using EVP_PKEY_free_ptr = std::unique_ptr<EVP_PKEY, decltype(&::EVP_PKEY_free)>;
 
 EVP_PKEY_free_ptr GenerateKeyPair();
-EVP_PKEY_free_ptr ExtractPublicKey(EVP_PKEY *private_key);
-DerivedData *DeriveSharedSecret(EVP_PKEY *public_key, EVP_PKEY *private_key);
+EVP_PKEY_free_ptr ExtractPublicKey(EVP_PKEY *);
+DerivedData *DeriveSharedSecret(EVP_PKEY *, EVP_PKEY *);
 
 }  // namespace networking_utility
 
