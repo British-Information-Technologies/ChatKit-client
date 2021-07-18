b54c28e7cecab1cf35bc7134811456b0b1888e62
mitch161 <mitchellhardie1@gmail.com>
2021-07-18 12:02:02 +0100

    Added tests for de/serialization of json
    These tests have not been pushed in an incremental fashion due to the countless issues that occured when trying to setup this section of openssl.

--- test/model/networking/utility/elliptic-curve-diffiehellman_test.cc
+++ test/model/networking/utility/elliptic-curve-diffiehellman_test.cc
@@ -1,6 +1,7 @@
 #include "model/networking/utility/elliptic-curve-diffiehellman.h"
 
 #include <gtest/gtest.h>
+#include <openssl/pem.h>
 
 using namespace networking_utility;
 
@@ -78,4 +79,183 @@ TEST(EllipticCurveDiffieHellmanTest, CreateSharedSecret) {
 
   BN_free(secret_alice_BN);
   BN_free(secret_bob_BN);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, SerializeKey) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+
+  std::string key = SerializePublicKey(alice_public_key.get());
+
+  std::cout << key << std::endl;
+}
+
+TEST(EllipticCurveDiffieHellmanTest, SerializeDeserializeKey) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+
+  std::string key = SerializePublicKey(alice_public_key.get());
+
+  std::cout << "json: " << key << std::endl;
+
+  EVP_PKEY_free_ptr public_key = DeserializePublicKey(key.c_str());
+
+  /* Output keys in readable format */
+  std::string key_one = SerializePublicKey(alice_public_key.get());
+  std::string key_two = SerializePublicKey(public_key.get());
+  std::cout << "key one: " << key_one << std::endl;
+  std::cout << "key two: " << key_two << std::endl;
+
+  EXPECT_EQ(key_one, key_two);
+  EXPECT_EQ(key_one, key);
+  EXPECT_EQ(key_two, key);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, DeriveMatchingSecret) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+  EXPECT_NE(alice_private_key, nullptr);
+  EXPECT_NE(bob_private_key, nullptr);
+
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+  EVP_PKEY_free_ptr bob_public_key = ExtractPublicKey(bob_private_key.get());
+
+  EXPECT_NE(alice_public_key, nullptr);
+  EXPECT_NE(bob_public_key, nullptr);
+
+  std::string alice_key = SerializePublicKey(alice_public_key.get());
+  std::cout << "alice: " << alice_key << std::endl;
+
+  std::string bob_key = SerializePublicKey(bob_public_key.get());
+  std::cout << "bob: " << bob_key << std::endl;
+
+  EXPECT_NE(bob_key, alice_key);
+
+  EVP_PKEY_free_ptr alice_peer_key = DeserializePublicKey(bob_key.c_str());
+  EVP_PKEY_free_ptr bob_peer_key = DeserializePublicKey(alice_key.c_str());
+
+  DerivedData* secret_alice =
+      DeriveSharedSecret(alice_peer_key.get(), alice_private_key.get());
+  DerivedData* secret_bob =
+      DeriveSharedSecret(bob_peer_key.get(), bob_private_key.get());
+
+  std::cout << "alice secret: " << *(secret_alice->secret) << std::endl;
+  std::cout << "bob secret: " << *(secret_bob->secret) << std::endl;
+  std::cout << "alice secret length: " << secret_alice->length << std::endl;
+  std::cout << "bob secret length: " << secret_bob->length << std::endl;
+
+  EXPECT_EQ(*(secret_alice->secret), *(secret_bob->secret));
+  EXPECT_EQ(secret_alice->length, secret_bob->length);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, DeriveMatchingSecretDouble) {
+  EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+  EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+  EXPECT_NE(alice_private_key, nullptr);
+  EXPECT_NE(bob_private_key, nullptr);
+
+  EVP_PKEY_free_ptr alice_public_key =
+      ExtractPublicKey(alice_private_key.get());
+  EVP_PKEY_free_ptr bob_public_key = ExtractPublicKey(bob_private_key.get());
+
+  EXPECT_NE(alice_public_key, nullptr);
+  EXPECT_NE(bob_public_key, nullptr);
+
+  std::string alice_key = SerializePublicKey(alice_public_key.get());
+  std::cout << "alice: " << alice_key << std::endl;
+
+  std::string bob_key = SerializePublicKey(bob_public_key.get());
+  std::cout << "bob: " << bob_key << std::endl;
+
+  EXPECT_NE(bob_key, alice_key);
+
+  EVP_PKEY_free_ptr alice_peer_key = DeserializePublicKey(bob_key.c_str());
+  EVP_PKEY_free_ptr bob_peer_key = DeserializePublicKey(alice_key.c_str());
+
+  DerivedData* secret_alice =
+      DeriveSharedSecret(alice_peer_key.get(), alice_private_key.get());
+  DerivedData* secret_bob =
+      DeriveSharedSecret(bob_peer_key.get(), bob_private_key.get());
+
+  std::cout << "alice secret: " << *(secret_alice->secret) << std::endl;
+  std::cout << "bob secret: " << *(secret_bob->secret) << std::endl;
+  std::cout << "alice secret length: " << secret_alice->length << std::endl;
+  std::cout << "bob secret length: " << secret_bob->length << std::endl;
+
+  EXPECT_EQ(*(secret_alice->secret), *(secret_bob->secret));
+  EXPECT_EQ(secret_alice->length, secret_bob->length);
+
+  alice_key = SerializePublicKey(alice_public_key.get());
+  std::cout << "alice: " << alice_key << std::endl;
+
+  bob_key = SerializePublicKey(bob_public_key.get());
+  std::cout << "bob: " << bob_key << std::endl;
+
+  EXPECT_NE(bob_key, alice_key);
+
+  alice_peer_key = DeserializePublicKey(bob_key.c_str());
+  bob_peer_key = DeserializePublicKey(alice_key.c_str());
+
+  secret_alice =
+      DeriveSharedSecret(alice_peer_key.get(), alice_private_key.get());
+  secret_bob = DeriveSharedSecret(bob_peer_key.get(), bob_private_key.get());
+
+  std::cout << "alice secret: " << *(secret_alice->secret) << std::endl;
+  std::cout << "bob secret: " << *(secret_bob->secret) << std::endl;
+  std::cout << "alice secret length: " << secret_alice->length << std::endl;
+  std::cout << "bob secret length: " << secret_bob->length << std::endl;
+
+  EXPECT_EQ(*(secret_alice->secret), *(secret_bob->secret));
+  EXPECT_EQ(secret_alice->length, secret_bob->length);
+}
+
+TEST(EllipticCurveDiffieHellmanTest, DeriveMatchingSecretMany) {
+  for (int i = 0; i < 100; ++i) {
+    EVP_PKEY_free_ptr alice_private_key = GenerateKeyPair();
+    EVP_PKEY_free_ptr bob_private_key = GenerateKeyPair();
+
+    EXPECT_NE(alice_private_key, nullptr);
+    EXPECT_NE(bob_private_key, nullptr);
+
+    EVP_PKEY_free_ptr alice_public_key =
+        ExtractPublicKey(alice_private_key.get());
+    EVP_PKEY_free_ptr bob_public_key = ExtractPublicKey(bob_private_key.get());
+
+    EXPECT_NE(alice_public_key, nullptr);
+    EXPECT_NE(bob_public_key, nullptr);
+
+    std::string alice_key = SerializePublicKey(alice_public_key.get());
+    std::cout << "alice: " << alice_key << std::endl;
+
+    std::string bob_key = SerializePublicKey(bob_public_key.get());
+    std::cout << "bob: " << bob_key << std::endl;
+
+    EXPECT_NE(bob_key, alice_key);
+
+    EVP_PKEY_free_ptr alice_peer_key = DeserializePublicKey(bob_key.c_str());
+    EVP_PKEY_free_ptr bob_peer_key = DeserializePublicKey(alice_key.c_str());
+
+    DerivedData* secret_alice =
+        DeriveSharedSecret(alice_peer_key.get(), alice_private_key.get());
+    DerivedData* secret_bob =
+        DeriveSharedSecret(bob_peer_key.get(), bob_private_key.get());
+
+    std::cout << "alice secret: " << *(secret_alice->secret) << std::endl;
+    std::cout << "bob secret: " << *(secret_bob->secret) << std::endl;
+    std::cout << "alice secret length: " << secret_alice->length << std::endl;
+    std::cout << "bob secret length: " << secret_bob->length << std::endl;
+
+    EXPECT_EQ(*(secret_alice->secret), *(secret_bob->secret));
+    EXPECT_EQ(secret_alice->length, secret_bob->length);
+
+    free(secret_alice->secret);
+    free(secret_bob->secret);
+    free(secret_alice);
+    free(secret_bob);
+  }
 }
