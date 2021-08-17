c476f1aa608ce7673cb94b0a6097e1c3e36f950b
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 19:24:15 +0100

    add test to check for Update type

--- test/model/message-functionality/message-node_test.cc
+++ test/model/message-functionality/message-node_test.cc
@@ -9,7 +9,7 @@ using namespace chat_client_model_message_functionality;
 TEST(MessageNodeTest, GetContent) {
   std::string content = "This is a message!";
 
-  MessageNode message(content);
+  MessageNode message(content, ClientStreamIn::Connect);
 
   EXPECT_EQ(message.GetContent(), content);
 }
@@ -18,8 +18,8 @@ TEST(MessageNodeTest, GetContentDouble) {
   std::string content_one = "one";
   std::string content_two = "two";
 
-  MessageNode message_one(content_one);
-  MessageNode message_two(content_two);
+  MessageNode message_one(content_one, ClientStreamIn::Connect);
+  MessageNode message_two(content_two, ClientStreamIn::Connect);
 
   EXPECT_EQ(message_one.GetContent(), content_one);
   EXPECT_EQ(message_two.GetContent(), content_two);
@@ -29,7 +29,7 @@ TEST(MessageNodeTest, GetContentMany) {
   for (int i = 0; i < 250; ++i) {
     std::string content = std::to_string(i);
 
-    MessageNode message(content);
+    MessageNode message(content, ClientStreamIn::Connect);
 
     EXPECT_EQ(message.GetContent(), content);
   }
@@ -38,7 +38,15 @@ TEST(MessageNodeTest, GetContentMany) {
 TEST(MessageNodeTest, GetTypeConnected) {
   std::string content = "one";
 
-  MessageNode message(content);
+  MessageNode message(content, ClientStreamIn::Connect);
 
   EXPECT_EQ(message.GetType(), ClientStreamIn::Connect);
+}
+
+TEST(MessageNodeTest, GetTypeUpdate) {
+  std::string content = "one";
+
+  MessageNode message(content, ClientStreamIn::Update);
+
+  EXPECT_EQ(message.GetType(), ClientStreamIn::Update);
 }
