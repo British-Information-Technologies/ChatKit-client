cb3d1c9faef1f19dedacccb68b032c64cdfc4acc
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 19:02:12 +0100

    add client inbound message enum
    This enum will contain all the message types that the client can recieve from a server. At the moment, this enum can only represent connect messages.

---
+++ src/model/message-functionality/client-stream-in.h
@@ -0,0 +1,8 @@
+#ifndef CPPCHATCLIENT_MODEL_MESSAGEFUNCTIONALITY_CLIENTSTREAMIN_
+#define CPPCHATCLIENT_MODEL_MESSAGEFUNCTIONALITY_CLIENTSTREAMIN_
+
+namespace chat_client_model_message_functionality {
+enum class ClientStreamIn { Connect };
+}
+
+#endif
