04b72e8abaec8bf7e5e6aaea3c06759444c0823b
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 18:19:59 +0100

    add message node with get content
    These changes have added a message node that will represent a message to be sent or recieved to and from a socket during a communication session. This will make it easier to handle messages, as well abstractly represent command types in the future.

---
+++ src/model/message-functionality/message-node.cc
@@ -0,0 +1,6 @@
+#include "message-node.h"
+
+using namespace std;
+using namespace chat_client_model_message_functionality;
+
+string MessageNode::GetContent() { return "This is a message!"; }
---
+++ src/model/message-functionality/message-node.h
@@ -0,0 +1,13 @@
+#ifndef CPPCHATCLIENT_MODEL_MESSAGEFUNCTIONALITY_MESSAGENODE_
+#define CPPCHATCLIENT_MODEL_MESSAGEFUNCTIONALITY_MESSAGENODE_
+
+#include "string"
+
+namespace chat_client_model_message_functionality {
+class MessageNode {
+ public:
+  std::string GetContent();
+};
+}  // namespace chat_client_model_message_functionality
+
+#endif
