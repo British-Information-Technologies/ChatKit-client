699a7688645e9ddd797e4158ea90508133bdaa7d
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 19:55:37 +0100

    add Disconnect command type

--- src/model/message-functionality/client-stream-in.h
+++ src/model/message-functionality/client-stream-in.h
@@ -2,7 +2,13 @@
 #define CPPCHATCLIENT_MODEL_MESSAGEFUNCTIONALITY_CLIENTSTREAMIN_
 
 namespace chat_client_model_message_functionality {
-enum class ClientStreamIn { Connect, Update, SendMessage, SendGlobalMessage };
+enum class ClientStreamIn {
+  Connect,
+  Update,
+  SendMessage,
+  SendGlobalMessage,
+  Disconnect
+};
 }
 
 #endif
