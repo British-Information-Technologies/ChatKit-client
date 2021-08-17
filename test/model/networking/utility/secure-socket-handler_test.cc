7d4980345ee5d41d0defa530b155b1dddb6fc20e
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 18:18:08 +0100

    add test to get the contents of a message node
    The purpose of this test is to ensure that the contents of a message node can be successfully retrieved.

--- test/model/networking/utility/secure-socket-handler_test.cc
+++ test/model/networking/utility/secure-socket-handler_test.cc
@@ -2,23 +2,39 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>
+
 #include "../temporary-client.h"
 #include "../temporary-server.h"
 
 #define IP "localhost"
 #define PORT "3490"
 
+using namespace networking_utility;
+
 class SecureSocketHandlerTest : public ::testing::Test {
  protected:
   TemporaryServer *server;
   TemporaryClient *client;
+  SecureSocketHandler *client_handler;
+  SecureSocketHandler *server_handler;
+  DerivedData *key;
 
   void SetUp() override {
     server = new TemporaryServer(IP, PORT);
     client = new TemporaryClient(IP, PORT);
+    key = (DerivedData *)malloc(sizeof(DerivedData));
+
+    key->length = 256;
+    key->secret = (unsigned char *)malloc(key->length);
+    key->secret = (unsigned char *)"300EE3B4C88D252280F8972B42EC4605A0499DE55248EC17D08F5CB8AC080B17300EE3B4C88D252280F8972B42EC4605A0499DE55248EC17D08F5CB8AC080B17300EE3B4C88D252280F8972B42EC4605A0499DE55248EC17D08F5CB8AC080B17300EE3B4C88D252280F8972B42EC4605A0499DE55248EC17D08F5CB8AC080B17";
 
     server->setup();
-    client->setup();
+    int client_sockfd = client->setup();
+    pthread_join(server->listener_id, NULL);
+
+    client_handler = new SecureSocketHandler(client_sockfd, key);
+    server_handler = new SecureSocketHandler(server->new_fd, key);
   }
 
   void TearDown() override {
@@ -27,5 +43,23 @@ class SecureSocketHandlerTest : public ::testing::Test {
 
     free(server);
     free(client);
+    free(key);
   }
 };
+
+TEST_F(SecureSocketHandlerTest, SendAndReadManyMessagesTest) {
+  secure_string message = "pog champ";
+
+  for (int i = 0; i < 250; ++i) {
+    client_handler->send(message);
+    secure_string result = server_handler->recv();
+    // std::cout << result << std::endl;
+    EXPECT_EQ(message, result);
+  }
+
+  // for (int i = 0; i < 250; ++i) {
+  //   secure_string result = server_handler->recv();
+  //   // std::cout << result << std::endl;
+  //   EXPECT_EQ(message, result);
+  // }
+}
