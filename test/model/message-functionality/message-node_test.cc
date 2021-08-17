ea31c343a6cf36e130f76590d934e067833fa305
mitch161 <mitchellhardie1@gmail.com>
2021-08-17 18:25:57 +0100

    add test to get two different message contents
    The purpose of this test is to create two separate message nodes and retrieve each of their contents, where each node represents a different message.

--- test/model/message-functionality/message-node_test.cc
+++ test/model/message-functionality/message-node_test.cc
@@ -7,7 +7,18 @@ using namespace chat_client_model_message_functionality;
 TEST(MessageNodeTest, GetContent) {
   std::string content = "This is a message!";
 
-  MessageNode message;
+  MessageNode message(content);
 
   EXPECT_EQ(message.GetContent(), content);
+}
+
+TEST(MessageNodeTest, GetContentDouble) {
+  std::string content_one = "one";
+  std::string content_two = "two";
+
+  MessageNode message_one(content_one);
+  MessageNode message_two(content_two);
+
+  EXPECT_EQ(message_one.GetContent(), content_one);
+  EXPECT_EQ(message_two.GetContent(), content_two);
 }
