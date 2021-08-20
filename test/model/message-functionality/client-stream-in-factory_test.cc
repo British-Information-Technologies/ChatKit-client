758e67ac9889bf2cce02a70a6dd4eae6450c64d9
mitch161 <mitchellhardie1@gmail.com>
2021-08-20 11:06:16 +0100

    add test to create connect command from data

---
+++ test/model/message-functionality/client-stream-in-factory_test.cc
@@ -0,0 +1,18 @@
+#include "model/message-functionality/client-stream-in-factory.h"
+
+#include <gtest/gtest.h>
+
+#include <nlohmann/json.hpp>
+
+using namespace chat_client_model_message_functionality;
+using json = nlohmann::json;
+
+TEST(ClientStreamInFactoryTest, GetConnectCommandTest) {
+  ClientStreamInFactory factory;
+
+  json json_object = {{"type", "Connect"}};
+
+  std::unique_ptr<Message> message = factory.GetMessage(json_object.dump());
+
+  EXPECT_EQ(message->ToString(), json_object.dump());
+}
