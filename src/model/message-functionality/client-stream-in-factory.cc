08fa232afb44ea0909640d94abc93ed48a0d131f
mitch161 <mitchellhardie1@gmail.com>
2021-08-20 12:07:46 +0100

    add new condition to extract json type to return correct Message type
    These changes have added a if condition to check the type of the message by converting it to json. This allows the factory to correctly return the corresponding connect or disconnected message type.

--- src/model/message-functionality/client-stream-in-factory.cc
+++ src/model/message-functionality/client-stream-in-factory.cc
@@ -1,12 +1,20 @@
 #include "client-stream-in-factory.h"
 
 #include "client-stream-in/connect-command.h"
+#include "client-stream-in/disconnected-command.h"
 
 using namespace chat_client_model_message_functionality;
 using namespace chat_client_model_message_functionality_client_stream_in;
+using json = nlohmann::json;
 
 std::unique_ptr<Message> ClientStreamInFactory::GetMessage(
     const std::string &plaintext) {
-  ConnectCommand connect_command;
-  return std::make_unique<ConnectCommand>(connect_command);
+  json plaintext_object = json::parse(plaintext);
+  std::string type = plaintext_object["type"];
+
+  if (type.compare("Connect") == 0) {
+    return std::make_unique<ConnectCommand>();
+  }
+
+  return std::make_unique<DisconnectedCommand>();
 }
