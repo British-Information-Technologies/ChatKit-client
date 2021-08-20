2e03559340b7dbd122c746501fa710a121f1fa0b
mitch161 <mitchellhardie1@gmail.com>
2021-08-20 09:37:21 +0100

    add test suite for network stream in request command

---
+++ test/model/message-functionality/network-stream-in/request-command_test.cc
@@ -0,0 +1,22 @@
+#include "model/message-functionality/network-stream-in/request-command.h"
+
+#include <gtest/gtest.h>
+
+#include <nlohmann/json.hpp>
+
+using namespace chat_client_model_message_functionality_network_stream_in;
+using json = nlohmann::json;
+
+TEST(RequestCommandTest, ToStringTest) {
+  RequestCommand command;
+
+  EXPECT_EQ(command.ToString(), "{\"type\":\"Request\"}");
+}
+
+TEST(RequestCommandTest, ToJsonTest) {
+  RequestCommand command;
+
+  json json_object = {{"type", "Request"}};
+
+  EXPECT_EQ(command.ToJson(), json_object);
+}
