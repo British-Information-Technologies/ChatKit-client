5baa7c8cc9ef8f2fb01196026412d9cb7c74ca5c
mitch161 <mitchellhardie1@gmail.com>
2021-08-21 14:58:05 +0100

    add overloading function that take an extra parameter for to and content commands

--- src/model/message-functionality/client-stream-out-factory.cc
+++ src/model/message-functionality/client-stream-out-factory.cc
@@ -2,6 +2,7 @@
 
 #include "client-stream-out/disconnect-command.h"
 #include "client-stream-out/send-global-message-command.h"
+#include "client-stream-out/send-message-command.h"
 
 using namespace chat_client_model_message_functionality;
 using namespace chat_client_model_message_functionality_client_stream_out;
@@ -15,4 +16,10 @@ std::unique_ptr<Message> ClientStreamOutFactory::GetMessage(
 std::unique_ptr<Message> ClientStreamOutFactory::GetMessage(
     const std::string &type, const std::string &content) {
   return std::make_unique<SendGlobalMessageCommand>(content);
+}
+
+std::unique_ptr<Message> ClientStreamOutFactory::GetMessage(
+    const std::string &type, const std::string &to,
+    const std::string &content) {
+  return std::make_unique<SendMessageCommand>(to, content);
 }
--- src/model/message-functionality/client-stream-out-factory.h
+++ src/model/message-functionality/client-stream-out-factory.h
@@ -9,7 +9,12 @@ namespace chat_client_model_message_functionality {
 class ClientStreamOutFactory {
  public:
   std::unique_ptr<Message> GetMessage(const std::string &type);
+
   std::unique_ptr<Message> GetMessage(const std::string &type,
+                                      const std::string &content);
+
+  std::unique_ptr<Message> GetMessage(const std::string &type,
+                                      const std::string &to,
                                       const std::string &content);
 };
 }  // namespace chat_client_model_message_functionality
