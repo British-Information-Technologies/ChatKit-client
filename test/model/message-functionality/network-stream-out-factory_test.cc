9e36b419160185a02385c57f19a855c2b5f61ac8
mitch161 <mitchellhardie1@gmail.com>
2021-08-21 15:29:14 +0100

    add test to get many connect command messages

--- test/model/message-functionality/network-stream-out-factory_test.cc
+++ test/model/message-functionality/network-stream-out-factory_test.cc
@@ -9,13 +9,33 @@ using json = nlohmann::json;
 
 TEST(NetworkStreamOutFactoryTest, GetConnectCommandTest) {
   NetworkStreamOutFactory factory;
+  std::string uuid = "1234";
+  std::string username = "mitch";
+  std::string address = "no address haha!";
 
   json json_object = {{"type", "Connect"},
-                      {"uuid", "1234"},
-                      {"username", "mitch"},
-                      {"address", "no address haha!"}};
+                      {"uuid", uuid},
+                      {"username", username},
+                      {"address", address}};
 
-  std::unique_ptr<Message> message = factory.GetMessage();
+  std::unique_ptr<Message> message =
+      factory.GetMessage(uuid, username, address);
 
   EXPECT_EQ(message->ToString(), json_object.dump());
+}
+
+TEST(NetworkStreamOutFactoryTest, GetManyConnectCommandTest) {
+  NetworkStreamOutFactory factory;
+
+  for (int i = 0; i < 250; ++i) {
+    json json_object = {{"type", "Connect"},
+                        {"uuid", std::to_string(i)},
+                        {"username", std::to_string(i)},
+                        {"address", std::to_string(i)}};
+
+    std::unique_ptr<Message> message = factory.GetMessage(
+        std::to_string(i), std::to_string(i), std::to_string(i));
+
+    EXPECT_EQ(message->ToString(), json_object.dump());
+  }
 }
