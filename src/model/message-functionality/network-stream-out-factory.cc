b9c2418992444c94955534b8b29a24f8d49e91f8
mitch161 <mitchellhardie1@gmail.com>
2021-08-21 15:29:44 +0100

    modify create method to take 3 parameters to make command unique

--- src/model/message-functionality/network-stream-out-factory.cc
+++ src/model/message-functionality/network-stream-out-factory.cc
@@ -6,6 +6,8 @@ using namespace chat_client_model_message_functionality;
 using namespace chat_client_model_message_functionality_network_stream_out;
 using json = nlohmann::json;
 
-std::unique_ptr<Message> NetworkStreamOutFactory::GetMessage() {
-  return std::make_unique<ConnectCommand>("1234", "mitch", "no address haha!");
+std::unique_ptr<Message> NetworkStreamOutFactory::GetMessage(
+    const std::string &uuid, const std::string &username,
+    const std::string &address) {
+  return std::make_unique<ConnectCommand>(uuid, username, address);
 }
--- src/model/message-functionality/network-stream-out-factory.h
+++ src/model/message-functionality/network-stream-out-factory.h
@@ -8,7 +8,9 @@
 namespace chat_client_model_message_functionality {
 class NetworkStreamOutFactory {
  public:
-  std::unique_ptr<Message> GetMessage();
+  std::unique_ptr<Message> GetMessage(const std::string &uuid,
+                                      const std::string &username,
+                                      const std::string &address);
 };
 }  // namespace chat_client_model_message_functionality
 
