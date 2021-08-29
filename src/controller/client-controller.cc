b74bb7fceb3ca2963014665df958446f526038bd
mitch161 <mitchellhardie1@gmail.com>
2021-08-29 15:26:34 +0100

    add send message observer for assignment

--- src/controller/client-controller.cc
+++ src/controller/client-controller.cc
@@ -7,9 +7,11 @@
 
 #include "observers/addfriend-observer.h"
 #include "observers/deletefriend-observer.h"
+#include "observers/send-message-observer.h"
 #include "view-factory.h"
 
 using namespace chat_client_controller;
+using namespace chat_client_controller_observers;
 using namespace chat_client_model;
 using namespace std;
 
@@ -27,6 +29,7 @@ void ClientController::Body() {
   // add observers to buttons
   AddFriendObserver add_friend_observer(model, view);
   DeleteFriendObserver delete_friend_observer(model, view);
+  SendMessageObserver send_message_observer(model, view);
 
   // enter main loop
   view->run();
