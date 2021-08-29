a6f6d7524eaddfbbb7916a4c6db097bbf3f5e62b
mitch161 <mitchellhardie1@gmail.com>
2021-08-29 15:26:03 +0100

    add test suite for key commands

---
+++ test/model/message-functionality/network-stream-out/encrypt-command_test.cc
@@ -0,0 +1,26 @@
+#include "model/message-functionality/network-stream-out/encrypt-command.h"
+
+#include <gtest/gtest.h>
+
+#include <nlohmann/json.hpp>
+
+using namespace chat_client_model_message_functionality_network_stream_out;
+using json = nlohmann::json;
+
+TEST(NetworkStreamOutEncryptCommandTest, ToStringTest) {
+  std::string key = "12134";
+
+  EncryptCommand command(key);
+
+  EXPECT_EQ(command.ToString(), "{\"key\":\"12134\",\"type\":\"Encrypt\"}");
+}
+
+TEST(NetworkStreamOutEncryptCommandTest, ToJsonTest) {
+  std::string key = "12134";
+
+  EncryptCommand command(key);
+
+  json json_object = {{"type", "Encrypt"}, {"key", key}};
+
+  EXPECT_EQ(command.ToJson(), json_object);
+}
