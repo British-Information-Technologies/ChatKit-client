1feda23fb5767e61ff8cb63193307fadbde3e599
mitch161 <mitchellhardie1@gmail.com>
2021-08-29 15:20:01 +0100

    add invalid type for all message comparisions

--- src/model/message-functionality/message.h
+++ src/model/message-functionality/message.h
@@ -1,15 +1,19 @@
-#ifndef MODEL_MESSAGEFUNCTIONALITY_MESSAGE_
-#define MODEL_MESSAGEFUNCTIONALITY_MESSAGE_
+#ifndef MODEL_MESSAGEFUNCTIONALITY_MESSAGE_H_
+#define MODEL_MESSAGEFUNCTIONALITY_MESSAGE_H_
 
 #include <nlohmann/json.hpp>
 #include <string>
 
 namespace chat_client_model_message_functionality {
+
+const std::string INVALID = "Invalid";
+
 class Message {
  public:
   virtual std::string ToString() = 0;
   virtual nlohmann::json ToJson() = 0;
 };
+
 }  // namespace chat_client_model_message_functionality
 
 #endif
