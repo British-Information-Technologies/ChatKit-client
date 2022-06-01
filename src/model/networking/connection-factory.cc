db893b129eb85d024048fefe04ba3307851313fc
mitch <mitchellhardie1@gmail.com>
2022-06-01 10:25:11 +0100

    remove client connection

--- src/model/networking/connection-factory.cc
+++ src/model/networking/connection-factory.cc
@@ -1,15 +1,13 @@
 #include "connection-factory.h"
 
 #include "server/server-connection.h"
-#include "client/client-connection.h"
 
 using namespace model_networking;
 using namespace model_networking_server;
-using namespace model_networking_client;
 
 std::shared_ptr<model_networking::Connection> ConnectionFactory::GetConnection(
     const int &type, const std::string &ip_address, const std::string &port) {
-      if(type) return std::make_shared<ClientConnection>(ip_address, port);
+      // if(type) return std::make_shared<ClientConnection>(ip_address, port);
       
       return std::make_shared<ServerConnection>(ip_address, port);
 }
