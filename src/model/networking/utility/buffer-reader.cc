abcd7bd32254253b7f1100ba2e7a822fc5ca6ffc
mitch <mitchellhardie1@gmail.com>
2023-04-30 20:03:19 +0100

    turn buffer reader class into function

--- src/model/networking/utility/buffer-reader.cc
+++ src/model/networking/utility/buffer-reader.cc
@@ -1,33 +1,25 @@
 #include "buffer-reader.h"
 
 #include <sys/socket.h>
+#include <string>
 
-using namespace model_networking_utility;
-
-BufferReader::BufferReader(int sockfd) {
-  this->sockfd = sockfd;
-  input_buffer = new char[buffer_size];
-  read_bytes = 0;
-  read_index = 0;
-}
-
-BufferReader::~BufferReader() { delete[] input_buffer; }
-
-std::string BufferReader::ReadLine() {
+std::string model_networking_utility::ReadBufferLine(int sockfd) {
   std::string payload;
+  
+  const int buffer_size = 1024;
+  char buffer[buffer_size];
+  
+  int read_bytes = 0, read_index = 0;
 
   while (1) {
-    if (read_index == 0 && read_bytes == 0) {
-      // This may need a timeout just incase some error occurs
-      read_bytes = recv(sockfd, input_buffer, buffer_size, 0);
-    }
+    read_bytes = recv(sockfd, buffer, buffer_size, 0);
 
+    // create string from buffer data (if no data read ignore)
     if (read_bytes > 0) {
       for (; read_index < read_bytes; ++read_index) {
-        char buffer_char = input_buffer[read_index];
+        char buffer_char = buffer[read_index];
 
         if (buffer_char == '\n') {
-          ++read_index;
           return payload;
         } else {
           payload += buffer_char;
@@ -36,9 +28,8 @@ std::string BufferReader::ReadLine() {
 
       read_index = 0;
       read_bytes = 0;
-    } else {
-      // Socket closed or socket error - needs clean up code
-      return "socket close or socket error!";
+    } else if (read_bytes == -1) {
+      // error - needs clean up code TODO
     }
   }
 }
--- src/model/networking/utility/buffer-reader.h
+++ src/model/networking/utility/buffer-reader.h
@@ -4,22 +4,7 @@
 #include <string>
 
 namespace model_networking_utility {
-class BufferReader {
- private:
-  int sockfd;
-
-  const int buffer_size = 1024;
-  char *input_buffer;
-  int read_bytes;
-  int read_index;
-
- public:
-  BufferReader(int);
-
-  ~BufferReader();
-
-  std::string ReadLine();
-};
+  std::string ReadBufferLine(int sockfd);
 }  // namespace model_networking_utility
 
 #endif
