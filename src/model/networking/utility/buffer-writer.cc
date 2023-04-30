a99cc05a331d5ed119653ed8501904a1b90dc479
mitch <mitchellhardie1@gmail.com>
2023-04-30 20:03:06 +0100

    turn buffer writer class into function

--- src/model/networking/utility/buffer-writer.cc
+++ src/model/networking/utility/buffer-writer.cc
@@ -1,12 +1,9 @@
 #include "buffer-writer.h"
 
 #include <sys/socket.h>
+#include <string>
 
-using namespace model_networking_utility;
-
-BufferWriter::BufferWriter(int sockfd) { this->sockfd = sockfd; }
-
-int BufferWriter::WriteLine(std::string message) {
+int model_networking_utility::WriteBufferLine(int sockfd, std::string message) {
   message.push_back('\n');
 
   int sent_bytes = send(sockfd, message.c_str(), message.length(), 0);
