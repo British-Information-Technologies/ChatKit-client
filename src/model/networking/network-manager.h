59285f7962a0153aef464ed2ce7f12889c28ae2d
mitch <mitchellhardie1@gmail.com>
2023-06-04 19:39:05 +0100

    replace thread class with thread lib for mutex compatibility

--- src/model/networking/network-manager.h
+++ src/model/networking/network-manager.h
@@ -6,19 +6,20 @@
 #include <memory>
 #include <event2/event.h>
 #include <nlohmann/json.hpp>
+#include <mutex>
 #include "msd/channel.hpp"
 
 #include "connection.h"
 #include "connection-factory.h"
 #include "messages/message.h"
 
-#include "../../../include/cpp-chat-client/thread.h"
-
 using json = nlohmann::json;
 
 namespace model {
-  class NetworkManager: public include::Thread {
+  class NetworkManager {
     private:
+      std::mutex connections_mutex;
+
       std::shared_ptr<struct event_base> connection_base;
       
       msd::channel<json> in_chann;
@@ -37,12 +38,6 @@ namespace model {
       int InitiateSecureConnection(const int &sockfd);
 
       int SendMessage(const int &id, std::string &data);
-
-    protected:
-      void InternalThreadEntry();
-
-    private:
-      void AnalyseIncomingMessage(int sockfd, Message *message);
   };
 }  // namespace model_networking
 
