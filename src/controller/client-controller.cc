8669e46f799b69a8dbb47f33de84de54bdb9479a
mitch <mitchellhardie1@gmail.com>
2023-06-04 19:43:22 +0100

    remove broken gtk4 gui for refactoring

--- src/controller/client-controller.cc
+++ src/controller/client-controller.cc
@@ -18,20 +18,8 @@ using namespace model;
 
 ClientController::ClientController(int argc, char **argv) {
   model = std::make_shared<ClientModel>();
-
-  ViewFactory factory;
-  view = factory.CreateView(argc, argv);
 }
 
 void ClientController::Body() {
-  // setup the gui
-  view->register_application();
-
-  // add observers to buttons
-  AddFriendObserver add_friend_observer(model, view);
-  DeleteFriendObserver delete_friend_observer(model, view);
-  SendMessageObserver send_message_observer(model, view);
-
-  // enter main loop
-  view->run();
+  model->Run();
 }
