6a49b90d8cce72f8fa5cdc5cacbe91bcd71ae660
mitch <mitchellhardie1@gmail.com>
2023-06-07 19:47:52 +0100

    add faked login button signal for loading main app

--- src/view/login/login-application-window.cc
+++ src/view/login/login-application-window.cc
@@ -1,11 +1,16 @@
 #include <gtkmm-4.0/gtkmm.h>
+#include <functional>
 
 #include "login-application-window.h"
 
 LoginApplicationWindow::LoginApplicationWindow(
     BaseObjectType *cobject, 
-    const Glib::RefPtr<Gtk::Builder> &refBuilder
+    const Glib::RefPtr<Gtk::Builder> &refBuilder,
+    std::function<void()> login_func
 ): Glib::ObjectBase("LoginApplicationWindow"), Gtk::ApplicationWindow(cobject)
-{}
+{
+    login_button = refBuilder->get_object<Gtk::Button>("loginButton");
+    login_button->signal_clicked().connect(login_func);
+}
 
 LoginApplicationWindow::~LoginApplicationWindow() {}
