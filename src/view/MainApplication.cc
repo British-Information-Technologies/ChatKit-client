56c203e28c9f5d5ab4a46a746c10bdb4b770cc0d
mitch <mitchellhardie1@gmail.com>
2023-06-07 11:27:07 +0100

    update to use derived login and home application windows

--- src/view/MainApplication.cc
+++ src/view/MainApplication.cc
@@ -1,7 +1,35 @@
 #include <gtkmm-4.0/gtkmm.h>
+#include <string>
 #include <iostream>
 
 #include "MainApplication.h"
+
+#include "login/login-application-window.h"
+#include "home/home-application-window.h"
+
+int MainApplication::UpdateBuilder(const std::string &filename) {
+  try {
+    if (!builder->add_from_file(filename)) {
+     printf("Builder failed to add_from_file\n");
+     return -1;
+    }
+
+  } catch (const Glib::FileError &ex) {
+    printf("FileError: %s\n", ex.what());
+    return -1;
+
+  } catch (const Glib::MarkupError &ex) {
+    printf("MarkupError: %s\n", ex.what());
+    return -1;
+
+  } catch (const Gtk::BuilderError &ex) {
+    printf("BuilderError: %s\n", ex.what());
+    return -1;
+
+  }
+
+  return 0;
+}
 
 MainApplication::MainApplication()
     : Gtk::Application("org.gtkmm.example") {
@@ -15,41 +43,61 @@ void MainApplication::on_startup() {
 }
 
 void MainApplication::on_activate() {
+  printf("T1");
+
   Gtk::Application::on_activate();
+  
+  printf("T2");
 
   builder = Gtk::Builder::create();
+  
+  printf("T3");
 
-  try {
-    if (!builder->add_from_file("view/login.ui")) {
-     printf("Builder failed to add_from_file\n");
-     return;
-    }
-
-  } catch (const Glib::FileError &ex) {
-    printf("FileError: %s\n", ex.what());
+  if(SetLoginWindow()) {
     return;
-
-  } catch (const Glib::MarkupError &ex) {
-    printf("MarkupError: %s\n", ex.what());
-    return;
-
-  } catch (const Gtk::BuilderError &ex) {
-    printf("BuilderError: %s\n", ex.what());
-    return;
-
   }
-
-  app_window = builder->get_widget<Gtk::ApplicationWindow>("rootAppWindow");
-
-  add_window(*app_window);
-  app_window->show();
 }
 
 void MainApplication::on_shutdown() {
   Gtk::Application::on_shutdown();
 
-  app_window->hide();
-  remove_window(*app_window);
+  if (app_window) {
+    remove_window(*app_window);
+  }
+}
+
+int MainApplication::SetLoginWindow() {
+  if (UpdateBuilder("view/login.ui")) {
+    return -1;
+  }
+
+  if (app_window) {
+    remove_window(*app_window);
+  }
+
+  app_window = builder->get_widget_derived<LoginApplicationWindow>(builder, "rootAppWindow");
+
+  add_window(*app_window);
+  app_window->show();
+
+  return 0;
+}
+
+int MainApplication::SetMainWindow() {
+  if (UpdateBuilder("view/home.ui")) {
+    return -1;
+  }
+
+  if (app_window) {
+    remove_window(*app_window);
+  }
+
+  app_window = builder->get_widget_derived<HomeApplicationWindow>(builder, "rootAppWindow");
+
+  add_window(*app_window);
+  app_window->show();
+
+  return 0;
 }
 
 Glib::RefPtr<MainApplication> MainApplication::create() {
