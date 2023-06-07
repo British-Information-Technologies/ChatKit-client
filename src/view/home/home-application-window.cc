08f8d305435f38fdc6746fd3726866e704903d4b
mitch <mitchellhardie1@gmail.com>
2023-06-07 20:17:29 +0100

    add fake send message signal

--- src/view/home/home-application-window.cc
+++ src/view/home/home-application-window.cc
@@ -1,4 +1,5 @@
 #include <gtkmm-4.0/gtkmm.h>
+#include <string>
 
 #include "home-application-window.h"
 
@@ -6,6 +7,22 @@ HomeApplicationWindow::HomeApplicationWindow(
     BaseObjectType *cobject, 
     const Glib::RefPtr<Gtk::Builder> &refBuilder
 ): Glib::ObjectBase("HomeApplicationWindow"), Gtk::ApplicationWindow(cobject)
-{}
+{
+    send_button = refBuilder->get_object<Gtk::Button>("sendButton");
+    send_button->signal_clicked().connect([this](){
+        if (msg_entry->get_text_length() == 0) {
+            return;
+        }
+
+        printf("Message to send: %s\n", msg_entry->get_text());
+
+        /* TODO
+        *  Need to send the msg to the model.
+        *  Not sure on best approach yet
+        */
+    });
+
+    msg_entry = refBuilder->get_object<Gtk::Entry>("msgEntry");
+}
 
 HomeApplicationWindow::~HomeApplicationWindow() {}
--- src/view/home/home-application-window.h
+++ src/view/home/home-application-window.h
@@ -4,6 +4,10 @@
 #include <gtkmm-4.0/gtkmm.h>
 
 class HomeApplicationWindow : public Gtk::ApplicationWindow {
+    private:
+        std::shared_ptr<Gtk::Button> send_button;
+        std::shared_ptr<Gtk::Entry> msg_entry;
+
     public:
         HomeApplicationWindow(BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &refBuilder);
         virtual ~HomeApplicationWindow();
