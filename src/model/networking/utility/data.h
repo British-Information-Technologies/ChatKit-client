39ba8f23d120dfaf992d8f7de51e3c676d989054
mitch <mitchellhardie1@gmail.com>
2023-08-11 15:01:11 +0100

    add struct for parsing internal thread data

---
+++ src/model/networking/utility/data.h
@@ -0,0 +1,15 @@
+#ifndef MODEL_NETWORKING_UTILITY_DATA_H_
+#define MODEL_NETWORKING_UTILITY_DATA_H_
+
+#include <memory>
+
+#include "model/networking/messages/message.h"
+
+namespace model{
+    struct Data {
+        int sockfd;
+        std::shared_ptr<Message> message;
+    };
+} // namespace model
+
+#endif
