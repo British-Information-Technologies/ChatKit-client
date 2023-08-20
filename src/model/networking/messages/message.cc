32b2e899601795f86f7e905f0c07df3831239f03
mitch <mitchellhardie1@gmail.com>
2023-08-20 10:01:59 +0100

    add public key base64 decode during deserialize

--- src/model/networking/messages/message.cc
+++ src/model/networking/messages/message.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <nlohmann/json.hpp>
 #include <memory>
+#include <sodium.h>
 
 #include "message.h"
 
@@ -35,6 +36,8 @@
 #include "stream-out/client/send-message.h"
 
 #include "internal/event-error.h"
+
+#include "model/networking/utility/variants.h"
 
 using namespace model;
 
@@ -145,8 +148,25 @@ int model::DeserializeServerStreamIn(Message* msg, std::string &data) {
         msg = new server_stream_in::Error(err_msg);
 
     } else if (type == server_stream_in::kPublicKey && data_json.contains("key")) {
-        std::string key = data_json.at("key");
-        msg = new server_stream_in::PublicKey(key);
+        std::string encoded_key = data_json.at("key");
+
+        const char* encoded_key_ptr = reinterpret_cast<const char*>(encoded_key.c_str());
+
+        size_t key_len = encoded_key.length() / 4 * 3;
+        unsigned char key_ptr[key_len];
+        
+        sodium_base642bin(
+            key_ptr,
+            key_len,
+            encoded_key_ptr,
+            sizeof encoded_key_ptr,
+            NULL,
+            &key_len,
+            NULL,
+            base64_VARIANT
+        );
+
+        msg = new server_stream_in::PublicKey(std::string(reinterpret_cast<char const*>(key_ptr), key_len));
 
     } else {
         msg = new internal::EventError("[DeserializeError]: Message is not StreamIn");
@@ -249,6 +269,28 @@ int model::DeserializeServerStreamOut(Message* msg, std::string &data) {
 
     } else if (type == server_stream_out::kDisconnect) {
         msg = new server_stream_out::Disconnect();
+
+    } else if (type == server_stream_out::kPublicKey) {
+        std::string to = data_json.at("to");
+        std::string encoded_key = data_json.at("key");
+
+        const char* encoded_key_ptr = reinterpret_cast<const char*>(encoded_key.c_str());
+
+        size_t key_len = encoded_key.length() / 4 * 3;
+        unsigned char key_ptr[key_len];
+        
+        sodium_base642bin(
+            key_ptr,
+            key_len,
+            encoded_key_ptr,
+            sizeof encoded_key_ptr,
+            NULL,
+            &key_len,
+            NULL,
+            base64_VARIANT
+        );
+
+        msg = new server_stream_out::PublicKey(to, std::string(reinterpret_cast<char const*>(key_ptr), key_len));
 
     } else {
         msg = new internal::EventError("[DeserializeError]: Message is not StreamOut");
