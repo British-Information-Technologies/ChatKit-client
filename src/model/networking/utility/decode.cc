5b42d98ea78d75967f36a2d9863719b288128050
mitch <mitchellhardie1@gmail.com>
2023-08-20 10:26:13 +0100

    add base64 encode and decode functions

---
+++ src/model/networking/utility/decode.cc
@@ -0,0 +1,24 @@
+#include <string>
+
+#include "decode.h"
+
+#include "model/networking/utility/variants.h"
+
+std::string model::Base642Bin(std::string &encoded_data) {
+    const char* encoded_data_ptr = reinterpret_cast<const char*>(encoded_data.c_str());
+
+    size_t data_len = encoded_data.length() / 4 * 3; // base64 encodes 3 bytes as 4 characters
+    unsigned char data_ptr[data_len];
+    sodium_base642bin(
+        data_ptr,
+        data_len,
+        encoded_data_ptr,
+        sizeof encoded_data_ptr,
+        NULL,
+        &data_len,
+        NULL,
+        base64_VARIANT
+    );
+    
+    return std::string(reinterpret_cast<char const*>(data_ptr), data_len);
+}
---
+++ src/model/networking/utility/encode.cc
@@ -0,0 +1,21 @@
+#include <string>
+
+#include "encode.h"
+
+#include "model/networking/utility/variants.h"
+
+std::string model::Bin2Base64(std::string &data) {
+    const unsigned char* data_ptr = reinterpret_cast<const unsigned char*>(data.c_str());
+    unsigned long long data_len = sizeof data_ptr;
+
+    char encoded_data[sodium_base64_ENCODED_LEN(data_len, base64_VARIANT)];
+    sodium_bin2base64(
+        encoded_data,
+        sizeof encoded_data,
+        data_ptr,
+        data_len,
+        base64_VARIANT
+    );
+
+    return encoded_data;
+}
