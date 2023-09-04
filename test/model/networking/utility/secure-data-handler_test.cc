771b4beda83389d4c9309ed6d06d33cbb56e4f59
mitch <mitchellhardie1@gmail.com>
2023-09-04 19:20:01 +0100

    test secure data handler

--- test/model/networking/utility/secure-data-handler_test.cc
+++ test/model/networking/utility/secure-data-handler_test.cc
@@ -6,23 +6,27 @@
 #include "model/networking/utility/secure-data-handler.h"
 
 class SecureDataHandlerTest : public ::testing::Test {
- protected:
-  void SetUp() override {
-  }
-
-  void TearDown() override {
-  }
-
   protected:
-    unsigned char fake_secret[20] = "fake_shared_secret\0";
+    model::SecureDataHandler *data_handler;
+    
+    std::string data;
+
+    void SetUp() override {
+      unsigned char *fake_secret = (unsigned char*) malloc(sizeof(unsigned char) * 20);
+      fake_secret = (unsigned char*)"fake_shared_secret\0";
+  
+      data_handler = new model::SecureDataHandler(fake_secret);
+
+      data = "this is test data";
+    }
+
+    void TearDown() override {
+      free(data_handler);
+    }
 };
 
-TEST_F(SecureDataHandlerTest, FormatSendTest) {
-  model::SecureDataHandler data_handler(fake_secret);
-
-  std::string data("this is test data");
-
-  std::string packet = data_handler.FormatSend(data);
+TEST_F(SecureDataHandlerTest, FormatSendTest) { 
+  std::string packet = data_handler->FormatSend(data);
 
   std::cout << "data: " << data << std::endl;
   std::cout << "packet: " << packet << std::endl;
@@ -31,18 +35,17 @@ TEST_F(SecureDataHandlerTest, FormatSendTest) {
 }
 
 TEST_F(SecureDataHandlerTest, FormatSendThenReadTest) {
-  model::SecureDataHandler data_handler(fake_secret);
-
-  std::string data("this is test data");
-
-  std::string packet = data_handler.FormatSend(data);
+  std::string packet = data_handler->FormatSend(data);
 
   std::cout << "data: " << data << std::endl;
   std::cout << "packet: " << packet << std::endl;
 
   EXPECT_STRNE(data.c_str(), packet.c_str());
 
-  std::string payload = data_handler.FormatRead(packet);
+  std::string payload = data_handler->FormatRead(packet);
+
+  std::cout << "original data: " << payload << std::endl;
   
   EXPECT_STREQ(data.c_str(), payload.c_str());
+  EXPECT_EQ(data.length(), payload.length());
 }
