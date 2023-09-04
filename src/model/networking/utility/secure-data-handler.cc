f70f80456b2b404e78bed9236dbc9f5d5d72b9b3
mitch <mitchellhardie1@gmail.com>
2023-09-04 06:32:18 +0100

    fix read and send logic to pass test

--- src/model/networking/utility/secure-data-handler.cc
+++ src/model/networking/utility/secure-data-handler.cc
@@ -23,16 +23,19 @@ DataHandlerType SecureDataHandler::GetType() {
 
 std::string SecureDataHandler::FormatSend(std::string &data) {
   // create nonce
-  unsigned char nonce[crypto_box_NONCEBYTES];
-  randombytes_buf(nonce, sizeof nonce);  
+  unsigned char nonce[crypto_box_NONCEBYTES + 1];
+  randombytes_buf(nonce, crypto_box_NONCEBYTES);
+  nonce[crypto_box_NONCEBYTES] = '\0';
 
   // encrypt message with shared secret
-  const unsigned char* data_ptr = reinterpret_cast<const unsigned char*>(data.c_str());
-  unsigned long long data_len = sizeof data_ptr;
+  unsigned long long data_len = data.length();
+  unsigned char data_ptr[data_len + 1];
+  strcpy((char*) data_ptr, data.c_str());
   
   unsigned long long ciphertext_len = crypto_box_MACBYTES + data_len;
-  unsigned char ciphertext[ciphertext_len];
-  if(crypto_box_easy_afternm(
+  unsigned char ciphertext[ciphertext_len + 1];
+
+  if (crypto_box_easy_afternm(
     ciphertext,
     data_ptr,
     data_len,
@@ -43,39 +46,38 @@ std::string SecureDataHandler::FormatSend(std::string &data) {
     return "";
   }
 
+  ciphertext[ciphertext_len] = '\0';
+
   // create packet
   std::string packet = json({
-    {"payload", std::string(reinterpret_cast<char const*>(ciphertext), ciphertext_len)},
-    {"nonce", nonce}
+    {"payload", Bin2Base64(ciphertext)},
+    {"nonce", Bin2Base64(nonce)}
   }).dump();
 
   // encode packet with base64
-  return Bin2Base64(packet);
+  return packet;
 }
 
-std::string SecureDataHandler::FormatRead(std::string &data) {
-  // decode packet with base64
-  std::string packet_str = Base642Bin(data);
-  
+std::string SecureDataHandler::FormatRead(std::string &data) { 
   // check packet format
-  json packet = json::parse(packet_str);
+  json packet = json::parse(data);
   
   if (!packet.contains("nonce") || !packet.contains("payload")) {
     // invalid packet - network error
     return "";
   }
 
-  // extract nonce from packet
-  const unsigned char* nonce = reinterpret_cast<const unsigned char*>(std::string(packet.at("nonce")).c_str());
+  // decode packet with base64
+  unsigned char *nonce = Base642Bin(packet.at("nonce"));  
 
-  // extract ciphertext from packet
-  const unsigned char* ciphertext = reinterpret_cast<const unsigned char*>(std::string(packet.at("payload")).c_str());
-  unsigned long long ciphertext_len = sizeof ciphertext;
+  unsigned char *ciphertext = Base642Bin(packet.at("payload"));  
+  unsigned long long ciphertext_len = std::strlen((char*) ciphertext);
 
   // decrypt ciphertext with shared secret
   unsigned long long plaintext_len = ciphertext_len - crypto_box_MACBYTES;
-  unsigned char plaintext[plaintext_len];
-  if(crypto_box_open_easy_afternm(
+  unsigned char plaintext[plaintext_len + 1];
+
+  if (crypto_box_open_easy_afternm(
     plaintext,
     ciphertext,
     ciphertext_len,
@@ -84,7 +86,15 @@ std::string SecureDataHandler::FormatRead(std::string &data) {
   ) != 0) {
     return "";
   }
+
+  plaintext[plaintext_len] = '\0';
+
+  free(nonce);
+  free(ciphertext);
   
   // cast to string with length specified to avoid losing data from array conversion
-  return std::string(reinterpret_cast<char const*>(plaintext), plaintext_len);
+  char plaintext_str[plaintext_len + 1];
+  strcpy(plaintext_str, (char *) plaintext);
+
+  return plaintext_str;
 }
