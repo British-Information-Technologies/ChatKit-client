d20fdf53294cde30569fca55f1942c485cc17977
mitch <mitchellhardie1@gmail.com>
2023-09-04 06:31:59 +0100

    test secure data handler

---
+++ test/model/networking/utility/secure-data-handler_test.cc
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <string>
+
+#include "model/networking/utility/secure-data-handler.h"
+
+class SecureDataHandlerTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+};
+
+TEST_F(SecureDataHandlerTest, FormatSendTest) {
+  model::SecureDataHandler data_handler("fake_shared_secret");
+
+  std::string data("this is test data");
+
+  std::string packet = data_handler.FormatSend(data);
+
+  std::cout << "data: " << data << std::endl;
+  std::cout << "packet: " << packet << std::endl;
+
+  EXPECT_STRNE(data.c_str(), packet.c_str());
+}
+
+TEST_F(SecureDataHandlerTest, FormatSendThenReadTest) {
+  model::SecureDataHandler data_handler("fake_shared_secret");
+
+  std::string data("this is test data");
+
+  std::string packet = data_handler.FormatSend(data);
+
+  std::cout << "data: " << data << std::endl;
+  std::cout << "packet: " << packet << std::endl;
+
+  EXPECT_STRNE(data.c_str(), packet.c_str());
+
+  std::string payload data_handler.FormatRead(packet);
+  
+  EXPECT_STREQ(data.c_str(), payload.c_str());
+}
