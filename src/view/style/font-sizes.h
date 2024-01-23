af297a7dfae0f319a061512a7325005c2614ff18
mitch <mitchellhardie1@gmail.com>
2024-01-23 13:08:19 +0000

    add missing header protectors

--- src/view/style/font-sizes.h
+++ src/view/style/font-sizes.h
@@ -1,5 +1,10 @@
+#ifndef VIEW_STYLE_FONT_SIZES_H_
+#define VIEW_STYLE_FONT_SIZES_H_
+
 namespace view {
 #define FONT_SMALL 14
 
 #define FONT_MEDIUM 18
 } // namespace view
+
+#endif
--- src/view/style/formats.h
+++ src/view/style/formats.h
@@ -1,3 +1,6 @@
+#ifndef VIEW_STYLE_FORMATS_H_
+#define VIEW_STYLE_FORMATS_H_
+
 #include <fmt/core.h>
 
 #include "view/style/string-modifiers.h"
@@ -7,3 +10,5 @@ inline std::string SpanWithSize(int size, const std::string& text) {
     return fmt::format("<span size='{}pt'>{}</span>", size, StripAngleBrackets(text));
 }
 } // namespace view
+
+#endif
